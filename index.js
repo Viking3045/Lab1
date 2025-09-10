b52f39aa85e6f6778b2c8745713797fbb56fdbbd
Vitaliy Stepanenko <114361282+Viking3045@users.noreply.github.com>
2025-09-10 09:26:51 +0300

    add2

--- index.js
+++ index.js
@@ -51,10 +51,10 @@ function inputLastName(event) {
   obj.lastName = lastName;
   // console.log("lastName",lastName)
 }
-function inputPlatoon() {
-  const platoon = selectPlatoon.value;
-  // console.log("start>>>>>>>>>>>>",platoon)
+function inputPlatoon(event) {
+  const platoon = event.target.value;
   obj.platoon = platoon;
+
 }
 
 // console.log("obj>>>",obj)
@@ -76,27 +76,23 @@ function allInformation() {
     obj.typeOfTree.length > 1
   ) {
     menu.innerHTML = "";
+    menu.classList = "styleMenu"
     const container = document.createElement("div")
     container.classList = "container"
     const section = document.createElement("div")
     section.classList = "section"
     section.append(container)
     const ul = document.createElement("ul");
+  
     container.append(ul)
     const li = document.createElement("li");
     ul.append(li);
     const video = document.createElement("video");
-    // video.src = "./Лабораторна 1.mp4";
-    const source = document.createElement("source")
-    source.src = "./Лабораторна 1.mp4"
+    video.src = "/Лабораторна 1.mp4";
     video.autoplay = "autoplay";
     video.classList = "video"
-    // video.width = "320";
-    // video.height = "300";
     video.muted = "muted";
-    // video.controls = "controls";
-    // video.textContent = " Ваш браузер не розпізнає video";
-    video.append(source);
+    video.textContent = " Ваш браузер не розпізнає video";
     li.append(video);
     menu.append(section);
 
@@ -105,8 +101,9 @@ function allInformation() {
     title.textContent = "Початкові данні";
     liData.append(title);
     const tree = document.createElement("ul");
+    tree.classList = "tankWhoAdd cont"
     const itemNameTree = document.createElement("li");
-    itemNameTree.classList = "list"
+    itemNameTree.classList = "list tankWhoAddItem content"
     itemNameTree.textContent = `Назва деревини: ${obj.typeOfTree}`;
     tree.append(itemNameTree);
 
@@ -123,7 +120,7 @@ const treeForTrees = obj.typeOfTree
     }
 // console.log("dsadasd", massTree)
     const itemDensity = document.createElement("li");
- itemDensity.classList = "list"
+ itemDensity.classList = "list tankWhoAddItem content"
 
     itemDensity.textContent = `Густина: ${massTree.density}г/см2`;
     tree.append(itemDensity);
@@ -131,16 +128,16 @@ const treeForTrees = obj.typeOfTree
 
     const itemSize = document.createElement("li");
     itemSize.textContent = "Розмір зразка:60*150*30 мм";
-    itemSize.classList = "list"
+    itemSize.classList = "list tankWhoAddItem content"
     tree.append(itemSize);
     const itemMass = document.createElement("li");
-    itemMass.classList = "list"
+    itemMass.classList = "list tankWhoAddItem content"
     itemMass.textContent = `Маса:${massTree.mass}г`;
     obj.firstMass = massTree.mass;
     tree.append(itemMass);
     const itemFirstTemperature = document.createElement("li");
-    itemFirstTemperature.classList = "list"
-    itemFirstTemperature.textContent = "Початкова температура:200Сг";
+    itemFirstTemperature.classList = "list tankWhoAddItem content"
+    itemFirstTemperature.textContent = "Початкова температура:200С";
     tree.append(itemFirstTemperature);
 
     liData.append(tree);
@@ -153,24 +150,28 @@ const treeForTrees = obj.typeOfTree
     const timerId = setTimeout(() => {
       const randomMass = (Math.random() * (100 - 50) + 50).toFixed(2);
       obj.secondMass = randomMass;
+         const tree2 = document.createElement("ul");
+        tree2.classList = "tankWhoAdd cont"
       const secondMass = document.createElement("li");
-      secondMass.classList = "secondMass"
-      secondMass.textContent = `маса після проведення експерименту: ${randomMass}`;
+      secondMass.classList = "secondMass tankWhoAddItem content"
+      secondMass.textContent = `Маса після проведення експерименту: ${randomMass}`;
       const randomTemperature = (Math.random() * (260 - 240) + 240).toFixed(2);
       obj.secondTemperature = randomTemperature;
       const secondTemperature = document.createElement("li");
-      secondTemperature.classList = "secondTemperature"
-      secondTemperature.textContent = `максимальна температура: ${randomTemperature}`;
+      secondTemperature.classList = "secondTemperature tankWhoAddItem content"
+      secondTemperature.textContent = `Максимальна температура: ${randomTemperature}`;
       const maximumTemperatureIncrease = document.createElement("li")
       const maxiTemp = (randomTemperature - 200).toFixed(2)
       obj.maximumTemperatureIncrease = maxiTemp
+      const tree3 = document.createElement("ul");
+      tree3.classList = "tankWhoAdd cont"
       maximumTemperatureIncrease.textContent = `Максимальний приріст температури ${maxiTemp}`
-      maximumTemperatureIncrease.classList = "maximumTemperatureIncrease"
+      maximumTemperatureIncrease.classList = "maximumTemperatureIncrease tankWhoAddItem content"
       const sampleMassLoss = document.createElement("li")
       const semplMass = (((massTree.mass-randomMass)/massTree.mass)*100).toFixed(2)
       obj.semplMassLost = semplMass;
       sampleMassLoss.textContent = `Втрата маси зразка  ${semplMass}`
-      sampleMassLoss.classList = "sampleMassLoss"
+      sampleMassLoss.classList = "sampleMassLoss tankWhoAddItem content"
       //Час досягнення максимальної температури
       const randomTime = Number((Math.random() * (300 - 20) + 20).toFixed());
       obj.time = randomTime
@@ -191,11 +192,13 @@ const treeForTrees = obj.typeOfTree
       }
 console.log("combusityIn", combusityIn)
       // combustibility.textContent = `За займистістю ${obj.typeOfTree} належить до ${}`
-   
-      final.append(secondMass);
-      final.append(secondTemperature);
-      final.append(maximumTemperatureIncrease)
-      final.append(sampleMassLoss)
+      tree2.append(secondMass)
+      tree2.append(secondTemperature)
+      final.append(tree2);
+      // final.append(secondTemperature);
+      tree3.append(maximumTemperatureIncrease)
+      tree3.append(sampleMassLoss)
+       final.append(tree3);
       const prepareReport = document.createElement("button");
       prepareReport.textContent = "Піготувати звіт";
       prepareReport.classList = "report  btn";
@@ -233,63 +236,74 @@ console.log("combusityIn", combusityIn)
         //   zeroInput.type="text"
 
         const firstQuestion = document.createElement("div");
+        // firstQuestion.overflow ="scroll"
         firstQuestion.classList = "firstInput"
         firstQuestion.textContent =
-          "1. Обладнання на якому визначали групи групи горючості будівельних матеріалів";
-        const firstInput = document.createElement("input");
+          "1. Ознаки настання граничного стану конструкції за межею вогнестійкості";
+        const firstInput = document.createElement("textarea");
         firstInput.classList = " initialData__input";
         firstInput.name = "firstInput"
-        // firstInput.rows = "1"
-        firstInput.type="text"
+        firstInput.rows = "3"
+
         const secondQuestion = document.createElement("div");
+        secondQuestion.classList = "firstInput"
         secondQuestion.textContent =
           "2. Згідно якого нормативного документу проводять випробування буд. матеріалів на визначення групи горючості?";
-        const secondInput = document.createElement("input");
+        const secondInput = document.createElement("textarea");
         secondInput.classList = "secondInput initialData__input";
         secondInput.name = "secondInput"
-        secondInput.type="text"
-        // secondInput.rows = "1"
+        // secondInput.type="text"
+        secondInput.rows = "3"
+
         const thirdQuestion = document.createElement("div");
+        thirdQuestion.classList = "firstInput"
         thirdQuestion.textContent =
           "3. Які показники пожежної небезпеки будівельних матеріалів ви знаєте?";
         const thirdInput = document.createElement("textarea");
         thirdInput.classList = "thirdInput initialData__input";
         thirdInput.name = "thirdInput"
         // thirdInput.type="text"
-        thirdInput.rows = "2"
+        thirdInput.rows = "3"
+
         const fourthQuestion = document.createElement("div");
+        fourthQuestion.classList = "firstInput"
         fourthQuestion.textContent =
-          "4. Які будівельні матеріали були випробувані на визначення групи горючості?";
+          "4. Дайте визначення поняття теплопровідність. ";
         const fourthInput = document.createElement("textarea");
         fourthInput.classList = "fourthInput initialData__input";
         fourthInput.name = "fourthInput"
         // fourthInput.type="text"
-        fourthInput.rows = "1"
+        fourthInput.rows = "3"
+
         const fifthQuestion = document.createElement("div");
+        fifthQuestion.classList = "firstInput"
         fifthQuestion.textContent =
-          "5. Класифікація матеріалів за групами горючості";
+          "5. Граничний стан за ознакою втрати несучої спроможності ";
         const fifthInput = document.createElement("textarea");
         fifthInput.classList = "fifthInput initialData__input";
         fifthInput.name = "fifthInput"
         // fifthInput.type="text"
-        fifthInput.rows = "2"
+        fifthInput.rows = "3"
         const sixthQuestion = document.createElement("div");
+          sixthQuestion.classList = "firstInput"
         sixthQuestion.textContent = "Висновок";
+        sixthQuestion.overflow = "scroll";
         const sixthInput = document.createElement("textarea");
         sixthInput.classList = "sixthInput initialData__input";
         sixthInput.name = "sixthInput"
         // sixthInput.type="text"
         sixthInput.rows = "5"
+
+    sixthInput.overflow = "scroll";
         const print = document.createElement("button");
-        print.textContent = "зберегти звіт";
+        print.textContent = "Зберегти звіт";
         print.type = "button";
         print.classList = "print btn";
         const p = document.createElement("p")
         p.textContent = 
 
 
-        // zeroQueshions.append(zeroInput);
-        // fieldset.append(zeroQueshions)
+
         firstQuestion.append(firstInput);
         fieldset.append(firstQuestion);
         secondQuestion.append(secondInput);
@@ -361,45 +375,161 @@ console.log("combusityIn", combusityIn)
           ){
             // console.log("objFinalInput", obj)
 // 
-const first = `
-Автор звіту  ${obj.name} ${obj.lastName} ${obj.middleName} 
-Данні по експерименту:
-Назва зразка: ${obj.typeOfTree} ;
-Розмір зразка: 60*150*30 мм
-Густина:${obj.density} г/см ;
-Початкова маса: ${obj.firstMass} г;
-Початкова температура: 200 ^C;
-Маса після проведення експерименту: ${obj.secondMass} г;
-Максимальна температура: ${obj.secondTemperature} ^C;
-Максимальний приріст температури: ${obj.maximumTemperatureIncrease} ^C;
-Втрата маси зразка: ${obj.semplMassLost} г;
 
-Відповіді на запитання:
-1. ${obj.first}
-2. ${obj.second}.
-3. ${obj.third}.
-4. ${obj.fourth}.
-5. ${obj.fifth}.
-Висновок: ${obj.sixth}.`
 
-const blob = new Blob([first], { type: 'text/plain' });
 
-// Створюємо URL для Blob
-const url = URL.createObjectURL(blob);
 
-// Створюємо елемент <a> для завантаження файлу
-const a = document.createElement('a');
-a.href = url;
-a.download = `Звіт ${obj.lastName}.txt`; // Назва файлу
 
-// Додаємо елемент до документа
-document.body.appendChild(a);
-// Симулюємо клік на елемент
-a.click();
-// Видаляємо елемент з документа
-document.body.removeChild(a);
-// Вивільняємо URL
-URL.revokeObjectURL(url);
+
+
+
+
+
+
+
+
+
+
+// const first = `
+// Автор звіту  ${obj.name} ${obj.lastName} ${obj.middleName} 
+// Взвод ${obj.platoon}
+// Данні по експерименту:
+// Назва зразка: ${obj.typeOfTree} ;
+// Розмір зразка: 60*150*30 мм
+// Густина:${obj.density} г/см ;
+// Початкова маса: ${obj.firstMass} г;
+// Початкова температура: 200 ^C;
+// Маса після проведення експерименту: ${obj.secondMass} г;
+// Максимальна температура: ${obj.secondTemperature} ^C;
+// Максимальний приріст температури: ${obj.maximumTemperatureIncrease} ^C;
+// Втрата маси зразка: ${obj.semplMassLost} г;
+
+// Відповіді на запитання:
+// 1. ${obj.first}
+// 2. ${obj.second}.
+// 3. ${obj.third}.
+// 4. ${obj.fourth}.
+// 5. ${obj.fifth}.
+// Висновок: ${obj.sixth}.`
+
+
+
+
+
+
+
+const first = document.createElement("div")
+const a = document.createElement("p")
+const a1 = document.createElement("span")
+a.textContent = ` Автор звіту : ${obj.name} ${obj.lastName} ${obj.middleName} `
+a.style.fontWeight =  "bold"
+const b = document.createElement("p")
+b.textContent = `Взвод :${obj.platoon} `
+const c = document.createElement("p")
+c.textContent = `Данні по експерименту`
+c.style.fontWeight =  "bold"
+c.style.textAlign = "center"
+const d = document.createElement("p")
+d.textContent = `Назва зразка: ${obj.typeOfTree};`
+const e = document.createElement("p")
+e.textContent = `Розмір зразка: 60*150*30 мм;`
+const  f= document.createElement("p")
+f.textContent = `Густина:${obj.density} г/см;`
+const g = document.createElement("p")
+g.textContent = `Початкова маса: ${obj.firstMass} г;`
+const l = document.createElement("p")
+l.textContent = `Маса після проведення експерименту: ${obj.secondMass} г;`
+const i = document.createElement("p")
+i.textContent = `Максимальна температура: ${obj.secondTemperature} ^C;`
+const k = document.createElement("p")
+k.textContent = `Максимальний приріст температури: ${obj.maximumTemperatureIncrease} ^C;`
+const m = document.createElement("p")
+m.textContent = `Втрата маси зразка: ${obj.semplMassLost} г;`
+const n = document.createElement("p")
+n.textContent = `Відповіді на запитання`
+n.style.fontWeight =  "bold"
+n.style.textAlign = "center"
+const p = document.createElement("p")
+p.textContent = ` 1. ${obj.first}.`
+const r = document.createElement("p")
+r.textContent = `2. ${obj.second}.`
+const s = document.createElement("p")
+s.textContent = `3. ${obj.third}.`
+const t = document.createElement("p")
+t.textContent = `4. ${obj.fourth}.`
+const u = document.createElement("p")
+u.textContent = `5. ${obj.fifth}.`
+const y = document.createElement("p")
+y.textContent = `Висновок: ${obj.sixth}.`
+y.style.fontWeight =  "bold"
+
+// Назва зразка: ${obj.typeOfTree} ;
+first.append(a)
+first.append(b)
+first.append(c)
+first.append(d)
+first.append(e)
+first.append(f)
+first.append(g)
+first.append(l)
+first.append(i)
+first.append(k)
+first.append(m)
+first.append(n)
+first.append(p)
+first.append(r)
+first.append(s)
+first.append(t)
+first.append(u)
+first.append(y)
+
+
+  const pro = `${first.outerHTML}`
+
+
+
+
+
+
+// const blob = new Blob([first], { type: 'text/plain' });
+
+// // Створюємо URL для Blob
+// const url = URL.createObjectURL(blob);
+
+// // Створюємо елемент <a> для завантаження файлу
+// const a = document.createElement('a');
+// a.href = url;
+// a.download = `Звіт ${obj.lastName}.txt`; // Назва файлу
+
+// // Додаємо елемент до документа
+// document.body.appendChild(a);
+// // Симулюємо клік на елемент
+// a.click();
+// // Видаляємо елемент з документа
+// document.body.removeChild(a);
+// // Вивільняємо URL
+// URL.revokeObjectURL(url);
+const header =  `<html xmlns:o='urn:schemas-microsoft-com:office:office'
+              xmlns:w='urn:schemas-microsoft-com:office:word'
+              xmlns='http://www.w3.org/TR/REC-html40'>
+        <head>
+          <meta charset="utf-8">
+          <style>
+            body { font-family: "Times New Roman", Times, serif; }
+          </style>
+        </head>
+        <body>`;
+         const footer = "</body></html>";
+   const source = header + pro + footer;
+const blob = new Blob([source],  { type: "application/msword;charset=utf-8;fonts=timesNewRoman" });
+   saveAs(blob, "document.doc");
+    
+
+
+
+
+
+    
 menu.innerHTML = ""
 const sectionRefresh = document.createElement("section")
 sectionRefresh.classList = "initialData refreshSection"
@@ -407,6 +537,7 @@ sectionRefresh.classList = "initialData refreshSection"
         containerRefresh.classList = "container"
 const h = document.createElement("h2")
 h.textContent = "Ваша робота збережена, відправте її викладачу"
+h.classList ="lastText"
 const refreshBtn = document.createElement("button")
 refreshBtn.classList = "btn"
 refreshBtn.textContent = "Провести експеримент знову"
@@ -424,7 +555,7 @@ menu.append(sectionRefresh)
           
         } 
       }
-    }, 1000);
+    }, 103000);
     ul.append(final);
     
   } else {
